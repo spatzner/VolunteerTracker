c7e1451e1351dcd7eab67aa12e5d06a1611c5600
Scott Patzner <scott.patzner@gmail.com>
2024-03-01 23:35:21 -0600

    Start EF model (3)

---
+++ VolunteerTracker.Repository/VolunteerTracker.Migrations.Local/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using VolunteerTracker.Repository;
+
+public class VolunteerContextFactory : IDesignTimeDbContextFactory<VolunteerContext>
+{
+    public VolunteerContext CreateDbContext(string[] args)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("EFMIGRATIONDB");
+        
+        if (string.IsNullOrEmpty(connectionString))
+            throw new InvalidOperationException(
+                "The connection string was not set in the 'EFMIGRATIONDB' environment variable.");
+
+        var optionsBuilder = new DbContextOptionsBuilder<VolunteerContext>().UseNpgsql(connectionString);
+
+        return new VolunteerContext(optionsBuilder.Options);
+    }
+}
