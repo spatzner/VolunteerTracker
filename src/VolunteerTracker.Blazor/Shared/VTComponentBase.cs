f337553ae615d8c34725650bcf3d45d8aed134c7
Scott Patzner <scott.patzner@gmail.com>
2024-03-28 08:10:02 -0500

    Loading modal with values (3)

---
+++ src/VolunteerTracker.Blazor/Shared/VTComponentBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+
+namespace VolunteerTracker.Blazor.Shared
+{
+    public class VTComponentBase : ComponentBase
+    {
+
+    }
+}
