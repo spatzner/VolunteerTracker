7afb7b112cab5b7ed9f685d9fc15a5f0cfd27dc8
Scott Patzner <scott.patzner@gmail.com>
2024-03-29 21:06:56 -0500

    begin fields for phone, addr (2)

--- src/VolunteerTracker.Blazor/Components/Pages/Entities/IndividualEdit.razor.cs
+++ src/VolunteerTracker.Blazor/Components/Pages/Entities/IndividualEdit.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
+using VolunteerTracker.Common.DataAnnotations;
 using VolunteerTracker.Repository;
 using VolunteerTracker.Repository.Entities;
 
@@ -8,8 +9,8 @@ namespace VolunteerTracker.Blazor.Components.Pages.Entities;
 public partial class IndividualEdit
 {
     [Parameter]
-    public required Guid PersonGuid { get; set; }
-    
+    public required Guid? PersonGuid { get; set; }
+
     [Parameter]
     public EventCallback OnClose { get; set; }
 
@@ -17,18 +18,26 @@ public partial class IndividualEdit
     public required VolunteerContext Context { get; set; }
 
     private Person _person = new();
-
+    
+    private readonly List<string> _phoneTypes = typeof(Phone).GetAllowedValues(nameof(Phone.Type));
+    private int _phoneIndex = 0;
     protected override void OnInitialized()
     {
-        _person = Context.Persons.Include(x => x.Emails).Include(x => x.Phones).Include(x => x.Address).FirstOrDefault(p => p.Id == PersonGuid) ?? new Person();
+        _person = PersonGuid.HasValue
+            ? Context.Persons.Include(x => x.Emails).Include(x => x.Phones).Include(x => x.Address).FirstOrDefault(p => p.Id == PersonGuid) ?? new Person()
+            : new Person();
 
         base.OnInitialized();
     }
 
     private void Save()
     {
+        if (!PersonGuid.HasValue)
+            Context.Persons.Add(_person);
+
         Context.SaveChanges();
     }
+
     private void Close()
     {
         OnClose.InvokeAsync();
---
+++ src/VolunteerTracker.Common/DataAnnotations/AttributeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolunteerTracker.Common.DataAnnotations
+{
+    public static class AttributeExtensions
+    {
+        public static List<string> GetAllowedValues(this Type type, string propertyName)
+        {
+            var typeProperty = type.GetProperty(propertyName);
+            var attribute =  (AllowedValuesAttribute)typeProperty.GetCustomAttributes(typeof(AllowedValuesAttribute),false).First();
+            var values = attribute.Values.Select(x => (string)x!).ToList();
+            return values;
+        }
+    }
+}
