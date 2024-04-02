f9f57187a3228f58be47e5f170a298db47a8f442
Scott Patzner <scott.patzner@gmail.com>
2024-04-01 21:09:25 -0500

    added form component borders (2)

---
+++ src/VolunteerTracker.Blazor/Components/Shared/VTPhoneEdit.razor.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+using VolunteerTracker.Common.DataAnnotations;
+using VolunteerTracker.Repository.Entities;
+
+namespace VolunteerTracker.Blazor.Components.Shared;
+
+public partial class VTPhoneEdit
+{
+    [Parameter]
+    public required ICollection<Phone> List { get; set; }
+
+    [Parameter]
+    public EventCallback<ICollection<Phone>> ListChanged { get; set; }
+
+    [CascadingParameter]
+    public EditContext EditContext { get; set; } = null!;
+
+    private readonly List<string> _phoneTypes = typeof(Phone).GetAllowedValues(nameof(Phone.Type));
+
+    private void DeletePhone(Guid phoneId)
+    {
+        List.Remove(List.First(x => x.Id == phoneId));
+        ListChanged.InvokeAsync(List);
+    }
+
+    private void AddPhone(MouseEventArgs e)
+    {
+        List.Add(new Phone { Type = _phoneTypes.First(), IsPrimary = !List.Any() });
+        ListChanged.InvokeAsync(List);
+    }
+
+    private void NumberChanged(Phone phone, string newValue)
+    {
+        if (phone.Number == newValue)
+            return;
+
+        phone.Number = newValue;
+
+        Validate(phone, nameof(phone.Number));
+
+        ListChanged.InvokeAsync(List);
+    }
+
+    private void MainChanged(Phone phone, bool newValue)
+    {
+        if (!newValue || phone.IsPrimary == newValue)
+            return;
+
+        phone.IsPrimary = true;
+        foreach (Phone p in List.Where(x => x.Id != phone.Id))
+        {
+            p.IsPrimary = false;
+        }
+
+        Validate(phone, nameof(phone.IsPrimary));
+        ListChanged.InvokeAsync(List);
+    }
+
+    private void TypeChanged(Phone phone, string newValue)
+    {
+        if (phone.Type == newValue)
+            return;
+
+        phone.Type = newValue;
+        Validate(phone, nameof(phone.Type));
+        ListChanged.InvokeAsync(List);
+    }
+
+    private void Validate(Phone phone, string propertyName)
+    {
+        var index = List.ToList().IndexOf(phone);
+        var fullPropertyName = $"{nameof(Person.Phones)}[{index}].{propertyName}";
+        var fieldIdentifier = new FieldIdentifier(EditContext.Model, fullPropertyName);
+
+        EditContext.NotifyFieldChanged(fieldIdentifier);
+        EditContext.Validate();
+    }
+}
