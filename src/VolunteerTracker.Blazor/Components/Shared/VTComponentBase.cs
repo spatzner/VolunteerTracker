833336b56516364e73341edee64bc99abfd9b2d7
Scott Patzner <scott.patzner@gmail.com>
2024-04-02 20:16:59 -0500

    added simple buttons, inheritance (1.5)

---
+++ src/VolunteerTracker.Blazor/Components/Shared/VTComponentBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace VolunteerTracker.Blazor.Components.Shared
+{
+    public class VTComponentBase : ComponentBase
+    {
+        [Parameter]
+        public string Class { get; set; } = string.Empty;
+
+        [Parameter]
+        public string? Id { get; set; }
+
+        [Parameter]
+        public string Label { get; set; } = string.Empty;
+
+        protected override void OnInitialized()
+        {
+            Id ??= Guid.NewGuid().ToString().Replace("-", "");
+        }
+    }
+}
---
+++ src/VolunteerTracker.Blazor/Components/Shared/VTElementBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace VolunteerTracker.Blazor.Components.Shared
+{
+    public abstract class VTElementBase : VTComponentBase
+    {
+        [Parameter]
+        public string ContainerClass { get; set; } = string.Empty;
+    }
+}
