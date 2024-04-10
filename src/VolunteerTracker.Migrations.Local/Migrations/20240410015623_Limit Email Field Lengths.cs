17172b57cbca2111d1e6c0f162de9ba733f13c42
Scott Patzner <scott.patzner@gmail.com>
2024-04-09 21:37:18 -0500

    Migration to fix unbound text lengths

---
+++ src/VolunteerTracker.Migrations.Local/Migrations/20240410015623_Limit Email Field Lengths.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VolunteerTracker.Migrations.Local.Migrations
+{
+    /// <inheritdoc />
+    public partial class LimitEmailFieldLengths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                schema: "Entities",
+                table: "Emails",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                schema: "Entities",
+                table: "Emails",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                schema: "Entities",
+                table: "Emails",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                schema: "Entities",
+                table: "Emails",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
