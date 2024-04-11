824b0f988cd63d4977c4b97e6b6d8bc15e5f16b0
Scott Patzner <scott.patzner@gmail.com>
2024-04-10 21:45:58 -0500

    More Individual Playwirght tests (2.5)

---
+++ src/Tests/VolunteerTracker.Tests.Common/TestType.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace VolunteerTracker.Tests.Common
+{
+    public static class TestType
+    {
+        public const string Unit = "Unit";
+        public const string Component = "Component";
+        public const string Integration = "Integration";
+    }
+}
--- src/VolunteerTracker.Blazor.PlaywrightTests/IndividualsPresentationTests.cs
+++ src/VolunteerTracker.Blazor.PlaywrightTests/IndividualsPresentationTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Playwright;
 using Microsoft.Playwright.MSTest;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using VolunteerTracker.Tests.Common;
 
 namespace VolunteerTracker.Blazor.PlaywrightTests;
 
@@ -8,6 +10,7 @@ public class IndividualsPresentationTests : PageTest
 {
     
     [TestMethod]
+    [TestCategory(TestType.Integration)]
     public async Task IndividualsTableHasHeadersVisible()
     {
         await Page.GotoAsync("https://localhost:7231/individuals");
@@ -20,6 +23,7 @@ public class IndividualsPresentationTests : PageTest
     }
 
     [TestMethod]
+    [TestCategory(TestType.Integration)]
     public async Task IndividualsTableHasFilters()
     {
         await Page.GotoAsync("https://localhost:7231/individuals");
@@ -30,6 +34,7 @@ public class IndividualsPresentationTests : PageTest
     }
 
     [TestMethod]
+    [TestCategory(TestType.Integration)]
     public async Task IndividualsTableHasRowButtons()
     {
         await Page.GotoAsync("https://localhost:7231/individuals");
@@ -39,6 +44,7 @@ public class IndividualsPresentationTests : PageTest
 
 
     [TestMethod]
+    [TestCategory(TestType.Integration)]
     public async Task IndividualsTableHasDataOnLoad()
     {
         await Page.GotoAsync("https://localhost:7231/individuals");
@@ -50,6 +56,7 @@ public class IndividualsPresentationTests : PageTest
     }
 
     [TestMethod]
+    [TestCategory(TestType.Integration)]
     public async Task IndividualsLoadTenRows()
     {
         await Page.GotoAsync("https://localhost:7231/individuals");
@@ -68,6 +75,7 @@ public class IndividualsPresentationTests : PageTest
 
 
     [TestMethod]
+    [TestCategory(TestType.Integration)]
     public async Task IndividualsTableHasPagination()
     {
         await Page.GotoAsync("https://localhost:7231/individuals");
@@ -78,7 +86,10 @@ public class IndividualsPresentationTests : PageTest
 [TestClass]
 public class IndividualsActionTests : PageTest
 {
+    //TODO: remove ignore
     [TestMethod]
+    [TestCategory(TestType.Integration)]
+    [Ignore]
     public async Task CanAddNewIndividual()
     {
         await Page.GotoAsync("https://localhost:7231/individuals");
@@ -113,17 +124,126 @@ public class IndividualsActionTests : PageTest
         await Page.GetByLabel("Address", new() { Exact = true }).FillAsync("CArmstrong@gmail.com");
         await Page.GetByRole(AriaRole.Button, new() { Name = "Save" }).ClickAsync();
 
-        await Expect(Page.GetByText("Armstrong", new() { Exact = true })).ToBeVisibleAsync();
-        await Expect(Page.GetByText("Cheddar")).ToBeVisibleAsync();
-        await Expect(Page.GetByRole(AriaRole.Cell, new() { Name = "1234 Main St, Ste 2 Provo," })).ToBeVisibleAsync();
-        await Expect(Page.GetByRole(AriaRole.Cell, new() { Name = "231-523-6998" })).ToBeVisibleAsync();
-        await Expect(Page.GetByRole(AriaRole.Cell, new() { Name = "CArmstrong@gmail.com" })).ToBeVisibleAsync();
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Armstrong");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Cheddar");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("1234 Main St, Ste 2Provo, Virginia, 33525");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("231-523-6998");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("CArmstrong@gmail.com");
 
     }
+
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task SearchByLastNameIsBeginsWith()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.GetByRole(AriaRole.Textbox).First.ClickAsync();
+        await Page.GetByRole(AriaRole.Textbox).First.FillAsync("bart");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Bartell");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Bartoletti");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Barton");
+    }
+
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task SearchByFirstNameIsBeginsWith()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.GetByRole(AriaRole.Textbox).Nth(1).ClickAsync();
+        await Page.GetByRole(AriaRole.Textbox).Nth(1).FillAsync("Gl");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Glen");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Gloria");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Glenna"); 
+    }
     
-    //TODO: each search field works
-    //TODO: Pagination works
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task SearchByPhoneNumberIsBeginsWithAndSearchesSecondaryNumbersToo()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.GetByRole(AriaRole.Textbox).Nth(2).ClickAsync();
+        await Page.GetByRole(AriaRole.Textbox).Nth(2).FillAsync("350");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("350-386-7619 x3792");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("350-241-9664");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "" }).Nth(2).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Dialog)).ToContainTextAsync("Home: 350-960-8912 x8815");
+    }
+
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task SearchByEmailIsBeginsWithAndSearchesSecondaryEmailsToo()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.GetByRole(AriaRole.Textbox).Nth(3).ClickAsync();
+        await Page.GetByRole(AriaRole.Textbox).Nth(3).FillAsync("marci");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Marcia_Von@yahoo.com");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "" }).First.ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Dialog)).ToContainTextAsync("Work: Marcia.Frami78@yahoo.com");
+    }
+
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task LastPageButtonGoesToLastPage()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.Locator("nav > ul > li:nth-child(10) > .page-link").ClickAsync(); 
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Zulauf");
+        await Expect(Page.GetByLabel("Pagination links").GetByRole(AriaRole.Button)).ToContainTextAsync("100");
+    }
+
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task FirstPageButtonGoesToFirstPage()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.Locator("nav > ul > li:nth-child(10) > .page-link").ClickAsync();
+        await Page.Locator(".page-link").First.ClickAsync();
+        await Expect(Page.GetByLabel("Pagination links").GetByRole(AriaRole.Button)).ToContainTextAsync("1");
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Abbott");
+    }
+    
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task NextPageButtonGoesToNextPage()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.Locator("li:nth-child(9) > .page-link").ClickAsync();
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Aufderhar");
+        await Expect(Page.GetByLabel("Pagination links").GetByRole(AriaRole.Button)).ToContainTextAsync("2");
+    }
+
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task PreviousPageButtonGoesToPreviousPage()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.GetByText("3", new() { Exact = true }).ClickAsync();
+        await Page.Locator("li:nth-child(2) > .page-link").ClickAsync();
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Aufderhar");
+        await Expect(Page.GetByLabel("Pagination links").GetByRole(AriaRole.Button)).ToContainTextAsync("2");
+    }
+    
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task ClickingPageNumberTakesYouToThatPage()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.GetByText("4", new() { Exact = true }).ClickAsync();
+        await Expect(Page.Locator("tbody")).ToContainTextAsync("Barton");
+        await Expect(Page.GetByLabel("Pagination links").GetByRole(AriaRole.Button)).ToContainTextAsync("4");
+    }
+
+    [TestMethod]
+    [TestCategory(TestType.Integration)]
+    public async Task PagingShowCurrentRecordsOutOfTotal()
+    {
+        await Page.GotoAsync("https://localhost:7231/individuals");
+        await Page.GetByText("4", new() { Exact = true }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Article)).ToContainTextAsync("31 - 40 of 997 items");
+    }
+}
+
     //TODO: View shows all data
     //TODO: Can Edit Each field, including add / delete child entities
     //TODO: Required fields are enforced
-}
+
